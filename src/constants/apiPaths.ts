c6195540242d0e0473819ad49fd9cd106457e6fc
Egor Semenov <egor_semenov@epam.com>
2023-10-14 22:01:41 +0400

    [task4] itegrate endpoints

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://eu25wzu3cj.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://eu25wzu3cj.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://eu25wzu3cj.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://eu25wzu3cj.execute-api.eu-west-1.amazonaws.com",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://eu25wzu3cj.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
