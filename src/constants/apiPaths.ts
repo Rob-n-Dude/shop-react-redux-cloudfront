2c05cade84eacd4595777d70510b5151f11bf198
Egor Semenov <egor_semenov@epam.com>
2023-10-22 16:31:02 +0400

    [task-5] fe integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://eu25wzu3cj.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://eu25wzu3cj.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://eu25wzu3cj.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://zycpcwgxz0.execute-api.eu-west-1.amazonaws.com",
   bff: "https://eu25wzu3cj.execute-api.eu-west-1.amazonaws.com",
   cart: "https://eu25wzu3cj.execute-api.eu-west-1.amazonaws.com/dev",
 };
